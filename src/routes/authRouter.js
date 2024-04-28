c2210c647841d2dc4f61e0806186b4d17fb7d645
Cristofer543 <152051773+Cristofer543@users.noreply.github.com>
2024-04-28 04:15:27 -0400

    Update authRouter.js
    New Featured

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,6 +1,8 @@
+// authRouter.js
 const express = require("express");
 const router = express.Router();
 const db = require("../services/db");
+const moment = require("moment");
 
 // Ruta de registro (GET)
 router.get("/register", (req, res) => {
@@ -10,11 +12,16 @@ router.get("/register", (req, res) => {
 // Ruta de registro (POST)
 router.post("/register", (req, res) => {
   const { username, email, password } = req.body;
-  const sql = `INSERT INTO usuarios (username, email, password) VALUES (?, ?, ?)`;
-  db.query(sql, [username, email, password], (err, result) => {
+  const createdAt = new Date();
+  const sql = `INSERT INTO usuarios (username, email, password, created_at) VALUES (?, ?, ?, ?)`;
+  db.query(sql, [username, email, password, createdAt], (err, result) => {
     if (err) {
       console.error("Error al registrar el usuario:", err.message);
-      res.redirect("/register");
+      if (err.code === 'ER_NO_DEFAULT_FOR_FIELD') {
+        res.render('register', { error: 'Por favor, proporciona una contraseña.' });
+      } else {
+        res.redirect("/register");
+      }
       return;
     }
     console.log("Usuario registrado correctamente");
@@ -30,7 +37,7 @@ router.get("/login", (req, res) => {
 // Ruta de inicio de sesión (POST)
 router.post("/login", (req, res) => {
   const { username, password } = req.body;
-  const sql = `SELECT * FROM usuarios WHERE username = ? AND password = ?`;
+  const sql = `SELECT *, TIMESTAMPDIFF(SECOND, created_at, NOW()) AS time_created FROM usuarios WHERE username = ? AND password = ?`;
   db.query(sql, [username, password], (err, results) => {
     if (err) {
       console.error("Error al iniciar sesión:", err.message);
@@ -39,8 +46,11 @@ router.post("/login", (req, res) => {
     }
     if (results.length > 0) {
       req.session.loggedin = true;
+      req.session.userId = results[0].id;
       req.session.username = username;
-      req.session.email = results[0].email; // Guarda el correo electrónico en la sesión
+      req.session.email = results[0].email;
+      req.session.createdAt = results[0].created_at;
+      req.session.timeCreated = results[0].time_created;
       res.redirect("/dashboard");
     } else {
       res.send(
@@ -62,13 +72,14 @@ router.get("/profile", (req, res) => {
     return res.redirect("/login");
   }
 
-  const { username, email, createdAt } = req.session;
-  res.render("profiles", { username, email, createdAt });
+  const { username, email, createdAt, timeCreated } = req.session;
+  const createdAtFormatted = moment(createdAt).format("DD/MM/YYYY HH:mm:ss");
+  const timeCreatedFormatted = moment.utc(timeCreated * 1000).format("HH:mm:ss");
+  res.render("profiles", { username, email, createdAtFormatted, timeCreatedFormatted });
 });
 
-
 // Ruta para actualizar el nombre de usuario
-router.post('/profile/update', (req, res) => {
+router.post('/profile/update-username', (req, res) => {
   if (!req.session.loggedin) {
     return res.redirect('/login');
   }
@@ -90,4 +101,48 @@ router.post('/profile/update', (req, res) => {
     }
   );
 });
+
+// Ruta para actualizar la contraseña
+router.post('/profile/update-password', (req, res) => {
+  if (!req.session.loggedin) {
+    return res.redirect('/login');
+  }
+
+  const { currentPassword, newPassword } = req.body;
+  const userId = req.session.userId;
+
+  db.query(
+    'SELECT password FROM usuarios WHERE id = ?',
+    [userId],
+    (err, results) => {
+      if (err) {
+        console.error('Error al obtener la contraseña actual:', err);
+        return res.redirect('/profile');
+      }
+
+      if (results.length === 0) {
+        return res.render('profiles', { error: 'No se encontró el usuario.' });
+      }
+
+      const currentPasswordHash = results[0].password;
+      if (currentPassword !== currentPasswordHash) {
+        return res.render('profiles', { error: 'La contraseña actual es incorrecta.' });
+      }
+
+      db.query(
+        'UPDATE usuarios SET password = ? WHERE id = ?',
+        [newPassword, userId],
+        (err, results) => {
+          if (err) {
+            console.error('Error al actualizar la contraseña:', err);
+            return res.redirect('/profile');
+          }
+
+          res.redirect('/profile');
+        }
+      );
+    }
+  );
+});
+
 module.exports = router;
