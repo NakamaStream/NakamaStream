6d6f2dc96fe8e14c941d4787146e5d4a3eeaeaf0
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-05-03 23:26:31 -0600

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const indexRouter = require('./src/routes/indexRouter');
 const authRouter = require('./src/routes/authRouter');
 const dashboardRouter = require('./src/routes/dashboardRouter');
 const adminRouter = require('./src/routes/adminRouter');
+const banRouter = require('./src/routes/banRauter');
 
 const db = require('./src/services/db');
 
@@ -28,49 +29,11 @@ app.use(bodyParser.json());
 app.use('/src/public/css', express.static('src/public/css', { 'Content-Type': 'text/css' }));
 app.use('/src/public/js', express.static('src/public/js', { 'Content-Type': 'text/javascript' }));
 
-app.use((req, res, next) => {
-  if (req.path !== "/login" && req.path !== "/check-ban") {
-    db.query(
-      "SELECT banned, ban_expiration FROM usuarios WHERE id = ?",
-      [req.session.userId || 0],
-      (err, results) => {
-        if (err) {
-          console.error("Error al obtener el estado de baneado:", err);
-          return res.redirect("/dashboard");
-        }
-
-        if (results.length > 0) {
-          const { banned, ban_expiration } = results[0];
-          if (banned) {
-            if (ban_expiration > new Date()) {
-              const banExpirationFormatted = moment(ban_expiration).format(
-                "DD/MM/YYYY HH:mm:ss"
-              );
-              return res.render("banned", {
-                message: "Has sido baneado temporalmente.",
-                banExpirationFormatted,
-              });
-            } else {
-              return res.render("banned", {
-                message: "Has sido baneado permanentemente.",
-                banExpirationFormatted: null,
-              });
-            }
-          }
-        }
-
-        next();
-      }
-    );
-  } else {
-    next();
-  }
-});
-
 app.use('/', indexRouter);
 app.use('/', authRouter);
 app.use('/', dashboardRouter);
 app.use('/', adminRouter);
+app.use('/', banRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
