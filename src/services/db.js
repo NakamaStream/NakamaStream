050a9b538566331c27092cff4641b07b94f72fdd
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-05-10 16:01:38 -0600

    Handle errors during the connection

--- src/services/db.js
+++ src/services/db.js
@@ -17,27 +17,38 @@ try {
 
   db.connect((err) => {
     if (err) {
-      console.error('\x1b[31mError de conexión a la base de datos:\x1b[0m', err.message);
+      console.error('\x1b[31mDatabase connection error:\x1b[0m', err.message);
       return;
     }
-    console.log('\x1b[32mConexión a la base de datos establecida\x1b[0m');
-    console.log('\x1b[33mInformación de la base de datos:\x1b[0m');
+    console.log('\x1b[32mDatabase connection established\x1b[0m');
+    console.log('\x1b[33mDatabase information:\x1b[0m');
     console.log('\x1b[33mHost:\x1b[0m', config.host);
-    console.log('\x1b[33mUsuario:\x1b[0m', config.user);
-    console.log('\x1b[33mBase de datos:\x1b[0m', config.name);
+    console.log('\x1b[33mUser:\x1b[0m', config.user);
+    console.log('\x1b[33mDatabase name:\x1b[0m', config.name);
 
-    // Consulta para obtener información sobre el uso de memoria
+    // Query to get memory usage information
     db.query('SHOW VARIABLES LIKE "innodb_buffer_pool_size"', (err, results) => {
       if (err) {
-        console.error('\x1b[31mError al obtener información de memoria:\x1b[0m', err.message);
+        console.error('\x1b[31mError getting memory information:\x1b[0m', err.message);
         return;
       }
-      const memorySize = results[0].Value / (1024 * 1024); // Convertir bytes a megabytes
-      console.log('\x1b[33mTamaño del buffer de la pool de InnoDB (MB):\x1b[0m', memorySize);
+      const memorySize = results[0].Value / (1024 * 1024); // Convert bytes to megabytes
+      console.log('\x1b[33mInnoDB buffer pool size (MB):\x1b[0m', memorySize);
     });
+  });
+
+  // Handle errors during the connection
+  db.on('error', (err) => {
+    console.error('\x1b[31mDatabase error:\x1b[0m', err.message);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('\x1b[31mAttempting to reconnect to the database...\x1b[0m');
+      db.connect();
+    } else {
+      throw err;
+    }
   });
 
   module.exports = db;
 } catch (error) {
-  console.error('\x1b[31mError al cargar la configuración desde el archivo:\x1b[0m', error.message);
+  console.error('\x1b[31mError loading configuration from file:\x1b[0m', error.message);
 }
