cdd2655f7edea83925e200de133d4b85f6d8e057
akit <149289426+Sstudiosdev@users.noreply.github.com>
2024-05-10 19:01:14 -0600

    Update indexRouter.js

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -5,4 +5,16 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
+router.get('/error', (req, res) => {
+  res.render('error');
+})
+
+router.get('/tos', (req, res) => {
+  res.render('tos');
+})
+
+router.get('/privacy', (req, res) => {
+  res.render('privacy');
+})
+
 module.exports = router;
