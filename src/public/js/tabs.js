239286e9f034e92523d01a46d09e677ec37ce5bb
AssDevCl <notstaffof@gmail.com>
2024-10-12 12:28:04 -0600

    tabs.js - bug fix Toggle FQA

--- src/public/js/tabs.js
+++ src/public/js/tabs.js
@@ -1,11 +1,12 @@
-$(document).ready(function(){
-    $(".tab-item").click(function(){
-        var tab_id = $(this).attr("data-tab");
-
-        $(".tab-item").removeClass("is-active");
-        $(".tab-panel").removeClass("is-active");
-
-        $(this).addClass("is-active");
-        $("#" + tab_id).addClass("is-active");
-    });
+// Toggle mobile menu
+document.getElementById('mobile-menu-button').addEventListener('click', function() {
+    document.getElementById('mobile-menu').classList.toggle('hidden');
 });
+
+// Toggle FAQ answers
+function toggleFAQ(element) {
+    const answer = element.nextElementSibling;
+    const arrow = element.querySelector('span:last-child');
+    answer.classList.toggle('hidden');
+    arrow.classList.toggle('rotate-180');
+}
