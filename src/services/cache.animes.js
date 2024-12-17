2d7d37d14119f1945b08774fee964b5602dc9c72
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-12-17 13:17:12 -0600

    cache.animes.js - Middleware cache

---
+++ src/services/cache.animes.js
@@ -0,0 +1,34 @@
+const cache = {}; // Caché en memoria
+const CACHE_DURATION = 10 * 60 * 1000; // Duración de la caché (10 minutos)
+
+// Middleware de caché
+function cacheAnimes(key) {
+    return (req, res, next) => {
+        if (cache[key] && cache[key].timestamp > Date.now() - CACHE_DURATION) {
+            console.log("Cache hit: ", key);
+            return res.json(cache[key].data); // Devuelve los datos cacheados
+        }
+
+        // Sobrescribir res.send para guardar en caché solo si la respuesta es JSON
+        res.sendResponse = res.send;
+        res.send = (body) => {
+            // Solo cachear si la respuesta es un JSON válido
+            try {
+                const isJsonResponse = body && body.startsWith("{") || body.startsWith("[");
+                if (isJsonResponse) {
+                    cache[key] = {
+                        timestamp: Date.now(),
+                        data: JSON.parse(body), // Guarda la respuesta parseada en la caché
+                    };
+                }
+            } catch (e) {
+                console.error("Error al parsear JSON para la caché", e);
+            }
+            res.sendResponse(body); // Envía la respuesta original
+        };
+        next();
+    };
+}
+
+
+module.exports = cacheAnimes;
